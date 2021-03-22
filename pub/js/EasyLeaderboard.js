9cc4aa880968930efd30e5ff88fa53f5b7c2e632
Yicheng Xue <yicheng.xue@mail.utoronto.ca>
2021-03-22 05:07:34 -0400

    Initialize

---
+++ pub/js/EasyLeaderboard.js
@@ -0,0 +1,6 @@
+"use strict"
+// This file can be found by a client request to /js/course.js which happens through
+// the <script> tag in course.html.
+// The static directory middleware gives the client access to this file.
+
+console.log('Loaded EasyLeaderboard.js from the server.')
---
+++ server.js
@@ -0,0 +1,15 @@
+/* server.js - Express server*/
+'use strict';
+const log = console.log
+log('Express server')
+
+const express = require('express')
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/pub')))
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+})
