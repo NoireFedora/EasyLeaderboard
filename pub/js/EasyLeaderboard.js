65566edeac761911923fc19afde977aeb912a8aa
Yicheng Xue <yicheng.xue@mail.utoronto.ca>
2021-03-27 11:38:05 -0400

    Update library

--- pub/js/EasyLeaderboard.js
+++ pub/js/EasyLeaderboard.js
@@ -11,14 +11,69 @@
         this.data = [];
         this.numRow = 0;
         this.maxRow = 10;
+        // Only for convenience
+        this.tableId = undefined;
+        this.categoryListId = undefined;
+        this.tableHeadId = undefined;
+        this.colGroupId = undefined;
     }
 
+    // Helper function for showing Button Dropdown
     function showCategory(id) {
         document.getElementById(id).classList.toggle("show");
     }
 
-    function changeText(id, text) {
-        document.getElementById(id).textContent = text;
+    // Helper function for Sorting target Leaderboard by given Category
+    function changeSorting(buttonId, text, tableId, categoryListId) {
+        // Change Button Text
+        document.getElementById(buttonId).textContent = text;
+        // Change the order of Table Body
+        var tableBody =  document.getElementById(tableId);
+        var categoryList =  document.getElementById(categoryListId);
+        var isSorting = true;
+        var needSorting, rows, x, y, i, index;
+        for (index = 0; index < categoryList.childElementCount; index++){
+            if (categoryList.children[index].innerText == text){
+                break;
+            }
+        }
+        while (isSorting) {
+            isSorting = false;
+            rows = tableBody.rows;
+            for (i = 0; i < rows.length - 1; i++){
+                needSorting = false;
+                x = rows[i].getElementsByTagName("TD")[index + 1];
+                y = rows[i + 1].getElementsByTagName("TD")[index + 1];
+                const xNum = parseFloat(x.innerHTML.toLowerCase());
+                const yNum = parseFloat(y.innerHTML.toLowerCase());
+                if (!isNaN(xNum) && !isNaN(yNum)){
+                    if (xNum < yNum){
+                        needSorting = true;
+                        break;
+                    }
+                }
+                else{
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()){
+                        needSorting = true;
+                        break;
+                    }
+                }
+            }
+            if (needSorting){
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                isSorting = true;
+            }
+        }
+        resetRanking(tableId);
+    }
+
+    // Helper function for reset the ranking of target Leaderboard
+    function resetRanking(tableId) {
+        var tableBody =  document.getElementById(tableId);
+        var rows = tableBody.rows;
+        for (var i = 0; i < rows.length; i++){
+            rows[i].getElementsByTagName("TD")[0].innerText = i + 1;
+        }
     }
 
     EasyLeaderboard.prototype = {
@@ -30,9 +85,9 @@
             this.id = id;
             this.title = title;
             this.category = category;
-            this.numCategory = category.length;
+            this.numCategory = this.category.length;
             this.data = data;
-            this.numRow = data.length;
+            this.numRow = this.data.length;
 
             const position = document.getElementById(querySelector);
             
@@ -72,11 +127,13 @@
             const categoryList = document.createElement('div');
             categoryList.className = "CategoryList";
             categoryList.setAttribute("id", "Leaderboard-" + id + "-CategoryList");
+            this.categoryListId = "Leaderboard-" + id + "-CategoryList";
             for (var i = 0; i < category.length; i++) {
                 const categoryName = document.createElement('a');
+                categoryName.setAttribute("id", "Leaderboard-" + id + "-CategoryList-" + this.category[i]);
                 const text = this.category[i]
-                // changeText will be changeSorting in the future
-                categoryName.onclick = function() {changeText("Leaderboard-" + id + "-CategoryButton", text)};
+                const index = i;
+                categoryName.onclick = function() {changeSorting("Leaderboard-" + id + "-CategoryButton", text, "Leaderboard-" + id + "-Data", "Leaderboard-" + id + "-CategoryList")};
                 const categoryText = document.createTextNode(category[i]);
                 categoryName.appendChild(categoryText);
                 categoryList.appendChild(categoryName);
@@ -93,6 +150,7 @@
             // -- Column Group
             const columnGroup = document.createElement('colgroup');
             columnGroup.setAttribute("id", "Leaderboard-" + id + "-ColumnGroup");
+            this.colGroupId = "Leaderboard-" + id + "-ColumnGroup";
             // --- Ranking
             const rankingCol = document.createElement('col');
             rankingCol.className = "RankingCol";
@@ -100,6 +158,7 @@
             // --- Other Columns
             for (var numCol = 0; numCol < this.numCategory; numCol++){
                 const newCol = document.createElement('col');
+                newCol.setAttribute("id", "Leaderboard-" + id + "-ColumnGroup-" + this.category[numCol])
                 columnGroup.appendChild(newCol);
             }
 
@@ -110,6 +169,7 @@
             const attributes = document.createElement('tr');
             attributes.className = "Attributes";
             attributes.setAttribute("id", "Leaderboard-" + id + "-Attributes");
+            this.tableHeadId = "Leaderboard-" + id + "-Attributes";
             const attributeRanking = document.createElement('th');
             attributeRanking.appendChild(document.createTextNode("Ranking"));
             attributes.appendChild(attributeRanking);
@@ -123,8 +183,11 @@
             scoreboard.appendChild(tableHead);
             // -- Data
             const tableBody = document.createElement('tbody');
+            tableBody.setAttribute("id", "Leaderboard-" + id + "-Data");
+            this.tableId = "Leaderboard-" + id + "-Data";
             for (var row = 0; row < this.data.length; row++) {
                 const newRow = document.createElement('tr');
+                newRow.setAttribute("id", "Leaderboard-" + id + "-Data-" + this.data[row]["id"]);
                 const newRanking = document.createElement('td');
                 const rankingText = document.createTextNode(row + 1);
                 newRanking.appendChild(rankingText);
@@ -143,11 +206,94 @@
 
             // Insert Leaderboard to html
             position.append(newLeaderboard);
+
+        },
+
+        // Insert new Rows with given Data in the last position
+        insertRows: function(data){
+
+            this.data = this.data.concat(data);
+            var count = this.numRow + 1;
+            this.numRow = this.data.length;
+
+            var table = document.getElementById(this.tableId);
+            for (var row = 0; row < data.length; row++){
+                var newRow = table.insertRow();
+                var newRanking = newRow.insertCell();
+                newRanking.innerText = count;
+                count++;
+                for (var col = 0; col < this.numCategory; col++){
+                    var newData = newRow.insertCell();
+                    newData.innerHTML = data[row][this.category[col]];
+                }
+            }
+
+        },
+
+        // Delete Row with given id
+        deleteRow: function(id){
+
+            for (var i = 0; i < this.data.length; i++){
+                if (this.data[i]["id"] == id){
+                    this.data.splice(i, 1);
+                }
+            }
+
+            this.numRow = this.data.length;
+            var target = document.getElementById(this.tableId + "-" + id);
+            target.remove();
+            resetRanking(this.tableId);
+
+        },
+
+        // Delete Category with given name
+        deleteCategory: function(category){
+
+            var index;
+            for (index = 0; index < this.category.length; index++){
+                if (this.category[index] == category){
+                    this.category.splice(index, 1);
+                    break;
+                }
+            }
+            this.numCategory = this.category.length;
+
+            var target = document.getElementById(this.categoryListId + "-" + category);
+            target.remove();
+
+            var colGroup = document.getElementById(this.colGroupId + "-" + category);
+            colGroup.remove();
+
+            var tableHead = document.getElementById(this.tableHeadId);
+            tableHead.deleteCell(index + 1);
+
+            var table = document.getElementById(this.tableId);
+            for (var row = 0; row < this.numRow; row++){
+                table.rows[row].deleteCell(index + 1);
+            }
+
+        },
+
+        // UpdateLeaderboard with current stored data
+        updateLeaderboard: function(){
+
+        },
+
+        hideRow: function(){
+
+        },
+
+        hideCategory: function(){
+
+        },
+
+        setMaximumRow: function(){
+
+        },
+
+        setLink: function(){
+            
         }
-
-
-
-
 
     }
 
--- pub/js/examples.js
+++ pub/js/examples.js
@@ -1,8 +1,26 @@
 
+const exampleData = [{"id": 1, "Name": "Gran", "Score": 10}, {"id": 2, "Name": "Djeeta", "Score": 20}, {"id": 3, "Name": "Lulia", "Score": 30}];
+const exampleNewData = [{"id": 4, "Name": "Vikala", "Score": 2020}, {"id": 5, "Name": "Catura", "Score": 2021}];
+
+
 function examples() {
-    const test = new EasyLeaderboard();
-    test.createLeaderboard("Example1", "Example1", "SampleLeaderboard", ["Name", "Score"], 
-                        [{"Name": "Gran", "Score": 20}, {"Name": "Djeeta", "Score": 10}]);
+    const example1 = new EasyLeaderboard();
+    example1.createLeaderboard("Example1", "Example1", "SampleLeaderboard", ["Name", "Score"], exampleData);
+    
+    const example2 = new EasyLeaderboard();
+    example2.createLeaderboard("Example2", "Example2", "SampleLeaderboard", ["Name", "Score"], exampleData);
+    example2.insertRows(exampleNewData);
+
+    const example3 = new EasyLeaderboard();
+    example3.createLeaderboard("Example3", "Example3", "SampleLeaderboard", ["Name", "Score"], exampleData);
+    example3.insertRows(exampleNewData);
+    example3.deleteRow(2);
+
+    const example4 = new EasyLeaderboard();
+    example4.createLeaderboard("Example4", "Example4", "SampleLeaderboard", ["Name", "Score"], exampleData);
+    example4.insertRows(exampleNewData);
+    example4.deleteRow(2);
+    example4.deleteCategory("Name");
 }
 
 examples();
